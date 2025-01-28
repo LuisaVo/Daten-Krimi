4e08711ca35c049babb78d3116914631e71f4c56
LuisaVo <vollmer.luisa@gmail.com>
2025-01-28 21:09:38 +0100

    revert

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/daten-krimi/',
+  base: '/Daten-Krimi/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
