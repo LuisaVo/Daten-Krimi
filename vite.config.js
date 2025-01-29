52caaa6902c3e6cbc929ff43f98b29b3cd79a989
LuisaVo <vollmer.luisa@gmail.com>
2025-01-29 17:29:57 +0100

    wed.

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,34 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/Daten-Krimi/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        vue(),
+        vueDevTools(),
+      ],
+      base: '/',
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+      },
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [
+        vue(),
+        vueDevTools(),
+      ],
+      base: '/Daten-Krimi/',
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+      },
+    }
+  }
 })
