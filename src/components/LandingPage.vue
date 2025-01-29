cd9be740a553b6541f6398d45d2966f8135bd349
LuisaVo <vollmer.luisa@gmail.com>
2025-01-29 22:53:46 +0100

    fixed routing

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,10 +4,10 @@
     <p>Your mission is to find the villain, their accomplice, and their hiding place.</p>
     <p>Complete all four tasks to solve the mystery!</p>
     <ul>
-      <li><router-link to="/task1">Task 1: Identify the Villain</router-link></li>
-      <li><router-link to="/task2">Task 2: Find the Accomplice</router-link></li>
-      <li><router-link to="/task3">Task 3: Locate the Hiding Place</router-link></li>
-      <li><router-link to="/task4">Task 4: Disable the Timer</router-link></li>
+      <li><router-link to="/Daten-Krimi/task1">Task 1: Identify the Villain</router-link></li>
+      <li><router-link to="/Daten-Krimi/task2">Task 2: Find the Accomplice</router-link></li>
+      <li><router-link to="/Daten-Krimi/task3">Task 3: Locate the Hiding Place</router-link></li>
+      <li><router-link to="/Daten-Krimi/task4">Task 4: Disable the Timer</router-link></li>
     </ul>
   </div>
 </template>
